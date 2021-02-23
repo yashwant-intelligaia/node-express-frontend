4cf5120e1df831a13fed7be7e46ae7695bbc6f31
Yashwant Rautela <yashwant.rautela@intelligaia.com>
2021-02-23 22:19:16 +0530

    routes for api added

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const appRoot = require('app-root-path');
 const app = express()
 const port = 8000
 app.use(express.static(path.join(appRoot.path, 'frontend')));
+app.get('/api', (req, res) => {
+  res.send('Hello World!')
+})
 app.get('*', (req, res) => {
     res.sendFile(path.join(appRoot.path, 'frontend/index.html'));
 });
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
